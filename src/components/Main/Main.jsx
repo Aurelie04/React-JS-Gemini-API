f450fbaee37ab4e57f88f4bbb98ef7730e9ed3dc
Aurelie04 <aurelienana25@gmail.com>
2024-07-18 16:43:16 +0200

    custumizing function to generate response

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -50,6 +50,9 @@ const Main = () => {
                       <img src={assets.gemini_icon} alt=''/>
                       {loading
                       ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
                       </div>
                       : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
                       }
